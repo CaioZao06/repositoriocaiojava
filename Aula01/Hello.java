0934b1c9d189f1477eb300ce3fa9e98e8ce90e82
CaioBack <ccdcback@minha.fag.edu.br>
2025-03-08 11:48:25 -0300

    Método holaquetal

--- Aula01/Hello.java
+++ Aula01/Hello.java
@@ -1,7 +1,13 @@
 package Aula01;
 
 public class Hello {
-        public static void main(String[] args) {
-            System.out.println("Hello World");
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        holaQueTal();
+    }
+
+    public static void holaQueTal() {
+        System.out.println("Hola que tal");
+    }
 }
+
